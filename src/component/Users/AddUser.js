522374b05a6c38aee71510b4a8618e1e49fb310a
sujanpoojary <sujanpoojary884@gmail.com>
2021-04-25 13:27:17 +0530

    initial setup in project - editorconfig and app setup

---
+++ src/component/Users/AddUser.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AddUser = (props) => {
+  return (
+    <form>
+      <label>Username</label>
+      <input id="username" type="text" />
+    </form>
+  );
+};
+
+export default AddUser;
